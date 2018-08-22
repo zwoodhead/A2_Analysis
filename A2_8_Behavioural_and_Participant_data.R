e4a0f26b7580697c166daf2d177b96cad8d8c6e3
zwoodhead <zoe.woodhead@psy.ox.ac.uk>
2018-08-22 11:32:52 +0100

    Improved the column selection lines (using grep instead of which) and added analysis of the number of omitted responses

--- A2_8_Behavioural_and_Participant_data.R
+++ A2_8_Behavioural_and_Participant_data.R
@@ -9,6 +9,7 @@
 library(xlsx) #install.packages(c("xlsx","dplyr"))
 require(dplyr)
 library(reshape2)
+library(stringr)
 
 dir<-("C:/Users/zwoodhead/Dropbox/Project A2/A2_Data/")
 
@@ -38,12 +39,7 @@ table(participant_info$Gender)
 ########################################################
 # Calculate stats for tasks A and D
 
-A_D_cols <- c(
-  which(colnames(behavioural_data) == "A1.Words"),
-  which(colnames(behavioural_data) == "A2.Words"),
-  which(colnames(behavioural_data) == "D1.Words"),
-  which(colnames(behavioural_data) == "D2.Words")
-)
+A_D_cols <- grep('*.Words', colnames(behavioural_data)) 
 
 A_D_data <- behavioural_data[ , c(1,A_D_cols)]
 A_D_data$Filename <- as.factor(A_D_data$Filename)
@@ -63,49 +59,50 @@ A_D_ANOVA <- aov(value ~ Session * Task
 summary(A_D_ANOVA)
 
 ########################################################
+# Count number of omitted trials (trials with no response) 
+# for each task
+
+Omission_cols <- grep('*.Omit', colnames(behavioural_data))
+
+Omission_data <- behavioural_data[ , Omission_cols]
+
+Omission_sum <- colSums(Omission_data, na.rm=TRUE)
+nevents <- c(15,15,15,15,90,90,90,45,90,90,90,45) # The number of events in each condition
+Omission_perc <- colSums(Omission_data, na.rm=TRUE) / (nevents*nsub) * 100
+
+
+########################################################
 # Create table of accuracy and rt data for tasks B, C, E 
 # and F (Table 1)
 
 Table1 <- data.frame(
-  'Task'     = c("B","B","C","C","E","E","F","F"),
-  'Session'  = c(1,2,1,2,1,2,1,2),
+  'Task'     = c("B","C","E","F","B","C","E","F"),
+  'Session'  = c(1,1,1,1,2,2,2,2),
   'Accuracy' = rep(1,8),
   'Acc_sd'   = rep(1,8),
   'RT'       = rep(1,8),
-  'RT_sd'   = rep(1,8)
+  'RT_sd'   = rep(1,8),
+  'Omit_perc' = Omission_perc[5:12]
 )
 
 Table1$Task    <- as.factor(Table1$Task)
 Table1$Session <- as.factor(Table1$Session)
 
-Acc_col_nums <- c(
-  which(colnames(behavioural_data) == 'B1.Acc'),
-  which(colnames(behavioural_data) == 'B2.Acc'),
-  which(colnames(behavioural_data) == 'C1.Acc'),
-  which(colnames(behavioural_data) == 'C2.Acc'),
-  which(colnames(behavioural_data) == 'E1.Acc'),
-  which(colnames(behavioural_data) == 'E2.Acc'),
-  which(colnames(behavioural_data) == 'F1.Acc'),
-  which(colnames(behavioural_data) == 'F2.Acc')
-)
+Acc_col_nums <- grep('*.Acc', colnames(behavioural_data))
+
 Table1$Accuracy <- colMeans(behavioural_data[Acc_col_nums], na.rm=TRUE)
 Table1$Acc_sd   <- apply(behavioural_data[Acc_col_nums], 2, sd, na.rm=TRUE)
 
-RT_col_nums <- c(
-  which(colnames(behavioural_data) == 'B1.RT'),
-  which(colnames(behavioural_data) == 'B2.RT'),
-  which(colnames(behavioural_data) == 'C1.RT'),
-  which(colnames(behavioural_data) == 'C2.RT'),
-  which(colnames(behavioural_data) == 'E1.RT'),
-  which(colnames(behavioural_data) == 'E2.RT'),
-  which(colnames(behavioural_data) == 'F1.RT'),
-  which(colnames(behavioural_data) == 'F2.RT')
-)
+RT_col_nums <- grep('*.RT', colnames(behavioural_data))
+
 Table1$RT <- colMeans(behavioural_data[RT_col_nums], na.rm=TRUE)
 Table1$RT_sd   <- apply(behavioural_data[RT_col_nums], 2, sd, na.rm=TRUE)
 
-########################################################
-# Test for differences between sessions
+# Re-order Table
+positions <- order(Table1$Task)
+Table1 <- Table1[positions, ]
+
+Table1
 
 
 
